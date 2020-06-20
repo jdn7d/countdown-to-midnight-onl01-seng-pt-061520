7f7c2105baa67ddb7d79f85dcee78397c9d0c644
Jessie Donovan <donovanajessie@gmail.com>
2020-06-20 18:33:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,13 @@
 
 def countdown(integer)
   while integer > 0
+    
     puts "#{integer} SECOND(S)!"
     integer -=1
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+sleep 1
 end
