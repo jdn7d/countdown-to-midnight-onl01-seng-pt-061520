921f72723881308a2b8ad73f2c62e267949c82f4
Jessie Donovan <donovanajessie@gmail.com>
2020-06-20 18:37:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@ def countdown(integer)
   while integer > 0
 
     puts "#{integer} SECOND(S)!"
-    def countdown_with_sleep(integer)
-    time = Time.now
+    def countdown_with_sleep(seconds)
+
     sleep(1.seconds)
     end
     integer -=1
